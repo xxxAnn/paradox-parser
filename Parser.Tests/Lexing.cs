26f42ed1616759791c08371b3be8b0a63a71c32d
rskhm <ann.mauduy.decius@gmail.com>
2025-03-07 17:40:15 -0500

    Update tests

---
+++ Parser.Tests/Lexing.cs
@@ -0,0 +1,27 @@
+﻿namespace Parser.Tests 
+{
+    using Paradox.Lexing;
+    using static Paradox.Lexing.Tokenizer;
+
+
+    public class UnitTest1
+    {
+        
+        [Fact]
+        public void TokenizationTest()
+        {
+            var result = Tokenizer.Tokenize("hello = { world }");
+            Console.WriteLine($"[{string.Join(", ", result.Select(t => t.ToString()))}]");
+            Assert.Equal(
+                [
+                    new Token(TokenType.Identifier, "hello"),
+                    new Token(TokenType.EqualSign),
+                    new Token(TokenType.BracketOpen),
+                    new Token(TokenType.Identifier, "world"),
+                    new Token(TokenType.BracketClose)
+                ],
+                result
+            );
+        }
+    }
+}
--- Parser/Lexing/Token.cs
+++ Parser/Lexing/Token.cs
@@ -58,5 +58,31 @@ namespace Paradox.Lexing
                 _ => throw new System.Exception("Unknown token type")
             };
         }
+
+        // override equality operator
+        public override bool Equals(object? obj)
+        {
+            if (obj is Token other)
+            {
+                return TokenValue switch
+                {
+                    Identifier i => other.TokenValue is Identifier oi && i.Type == oi.Type && i.Value == oi.Value,
+                    Literal l => other.TokenValue is Literal ol && l.Type == ol.Type,
+                    _ => false
+                };
+            }
+            return false;
+        }
+
+        // override hash code
+        public override int GetHashCode()
+        {
+            return TokenValue switch
+            {
+                Identifier i => i.Value.GetHashCode() ^ i.Type.GetHashCode(),
+                Literal l => l.Type.GetHashCode(),
+                _ => 0
+            };
+        }
     }
 }
